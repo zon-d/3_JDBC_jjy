62704bb6c514bfac80d18396378bbf50033d8b93
zon_d <zon_d@icloud.com>
2022-09-19 16:23:32 +0900

    20220919

---
+++ member_board/src/edu/kh/jdbc/main/run/MainRun.java
@@ -0,0 +1,17 @@
+package edu.kh.jdbc.main.run;
+
+import edu.kh.jdbc.main.view.MainView;
+
+public class MainRun {
+	public static void main(String[] args) {
+		new MainView().mainMenu();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
