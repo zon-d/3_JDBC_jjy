1a605b64f9eda3b78590b9048ecfebebd7bebebd
zon_d <zon_d@icloud.com>
2022-09-26 20:47:29 +0900

    20220926

--- member_board/src/edu/kh/jdbc/board/model/dao/BoardDAO.java
+++ member_board/src/edu/kh/jdbc/board/model/dao/BoardDAO.java
@@ -14,25 +14,26 @@ import java.util.Properties;
 import edu.kh.jdbc.board.model.vo.Board;
 
 public class BoardDAO {
-	
+
 	private Statement stmt;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
+
 	private Properties prop;
-	
+
 	public BoardDAO() {
 		try {
 			prop = new Properties();
 			prop.loadFromXML(new FileInputStream("board-query.xml"));
-			
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/** 게시글 목록 조회 DAO
+	/**
+	 * 게시글 목록 조회 DAO
+	 * 
 	 * @param conn
 	 * @return boardList
 	 * @throws Exception
@@ -40,27 +41,26 @@ public class BoardDAO {
 	public List<Board> selectAllBoard(Connection conn) throws Exception {
 		// 결과 저장용 변수 선언
 		List<Board> boardList = new ArrayList<>();
-		
-		
+
 		try {
 			// SQL ~
 			String sql = prop.getProperty("selectAllBoard");
-			
+
 			// Statement ~
 			stmt = conn.createStatement();
-			
+
 			// ResultSet에 저장된 값을 List 옮겨 담기
 			rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				int boardNo = rs.getInt("BOARD_NO");
 				String boardTitle = rs.getString("BOARD_TITLE");
 				String memberName = rs.getString("MEMBER_NM");
 				int readCount = rs.getInt("READ_COUNT");
 				String createDate = rs.getString("CREATE_DT");
 				int commentCount = rs.getInt("COMMENT_COUNT");
-				
+
 				Board board = new Board();
 				board.setBoardNo(boardNo);
 				board.setBoardTitle(boardTitle);
@@ -68,23 +68,24 @@ public class BoardDAO {
 				board.setReadCount(readCount);
 				board.setCreateDate(createDate);
 				board.setCommentCount(commentCount);
-				
+
 				boardList.add(board);
 
 			}
-			
-		}finally {
+
+		} finally {
 			// ~~
 			close(rs);
 			close(stmt);
 		}
-		
-		
+
 		// 조회 결과 반환
 		return boardList;
 	}
 
-	/** 게시글 상세 조회 DAO
+	/**
+	 * 게시글 상세 조회 DAO
+	 * 
 	 * @param conn
 	 * @param boardNo
 	 * @return board
@@ -93,106 +94,110 @@ public class BoardDAO {
 	public Board selectBoard(Connection conn, int boardNo) throws Exception {
 		// 결과 저장용 변수 선언
 		Board board = null;
-		
+
 		try {
 			// SQL 얻어오기
 			String sql = prop.getProperty("selectBoard");
-			
+
 			// Preparedstatement 생성
 			pstmt = conn.prepareStatement(sql);
-			
+
 			// ? 알맞은 값 대입
 			pstmt.setInt(1, boardNo);
-			
+
 			// SQL(SELECT) 수행 후 결과(ResultSet) 반환 받기
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) { // 조회 결과가 있을 경우
+
+			if (rs.next()) { // 조회 결과가 있을 경우
 				board = new Board(); // Board 객체 생성 == board는 null 아님
-				
-				board.setBoardNo(		rs.getInt	("BOARD_NO"));
-				board.setBoardTitle(	rs.getString("BOARD_TITLE"));
-				board.setBoardContent(	rs.getString("BOARD_CONTENT"));
-				board.setMemberName(	rs.getString("MEMBER_NM"));
-				board.setReadCount(		rs.getInt	("READ_COUNT"));
-				board.setCreateDate(	rs.getString("CREATE_DT"));
-				board.setMemberNo( rs.getInt("MEMBER_NO"));
+
+				board.setBoardNo(rs.getInt("BOARD_NO"));
+				board.setBoardTitle(rs.getString("BOARD_TITLE"));
+				board.setBoardContent(rs.getString("BOARD_CONTENT"));
+				board.setMemberName(rs.getString("MEMBER_NM"));
+				board.setReadCount(rs.getInt("READ_COUNT"));
+				board.setCreateDate(rs.getString("CREATE_DT"));
+				board.setMemberNo(rs.getInt("MEMBER_NO"));
 			}
-			
-		}finally {
+
+		} finally {
 			close(rs);
 			close(pstmt);
 		}
-		
+
 		return board; // 조회 결과
 	}
 
-	/** 조회 수 증가
+	/**
+	 * 조회 수 증가
+	 * 
 	 * @param conn
 	 * @param boardNo
 	 * @return result
 	 * @throws Exception
 	 */
-	public int increaseReadCount(Connection conn, int boardNo) throws Exception{
-		
+	public int increaseReadCount(Connection conn, int boardNo) throws Exception {
+
 		int result = 0;
-		
+
 		try {
 			String sql = prop.getProperty("increaseReadCount");
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1,  boardNo);
-			
+
+			pstmt.setInt(1, boardNo);
+
 			result = pstmt.executeUpdate();
-			
-			
-		}finally {
+
+		} finally {
 			close(rs);
 			close(pstmt);
 		}
-		
-		
+
 		return result;
 	}
 
-	/** 게시글 수정 DAO
+	/**
+	 * 게시글 수정 DAO
+	 * 
 	 * @param conn
 	 * @param board
 	 * @return result
 	 * @throws Exception
 	 */
 	public int updateBoard(Connection conn, Board board) throws Exception {
-		
+
 		int result = 0;
-		
+
 		try {
-			
+
 			String sql = prop.getProperty("updateBoard");
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, board.getBoardTitle());
 			pstmt.setString(2, board.getBoardContent());
 			pstmt.setInt(3, board.getBoardNo());
-			
+
 			result = pstmt.executeUpdate();
-			
-		}finally {
+
+		} finally {
 			close(pstmt);
 		}
-		
+
 		return result;
 	}
 
-	/** 게시글 삭제
+	/**
+	 * 게시글 삭제
+	 * 
 	 * @param conn
 	 * @param boardNo
 	 * @return result
 	 * @throws Exception
 	 */
-	public int deleteBoard(Connection conn, int boardNo) throws Exception{
-	
+	public int deleteBoard(Connection conn, int boardNo) throws Exception {
+
 		int result = 0;
 
 		try {
@@ -210,7 +215,121 @@ public class BoardDAO {
 
 		return result;
 	}
-	
-	
+
+	/**
+	 * 게시글 등록 DAO
+	 * 
+	 * @param conn
+	 * @param board
+	 * @return result
+	 * @throws Exception
+	 */
+	public int insertBoard(Connection conn, Board board) throws Exception {
+
+		int result = 0;
+
+		try {
+			String sql = prop.getProperty("insertBoard");
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, board.getBoardNo());
+			pstmt.setString(2, board.getBoardTitle());
+			pstmt.setString(3, board.getBoardContent());
+			pstmt.setInt(4, board.getMemberNo());
+
+			result = pstmt.executeUpdate();
+
+		} finally {
+			close(pstmt);
+		}
+
+		return result;
+	}
+
+	/**
+	 * 다음 게시글 번호 생성 DAO
+	 * 
+	 * @param conn
+	 * @return boardNo
+	 * @throws Exception
+	 */
+	public int nextBoardNo(Connection conn) throws Exception {
+		int boardNo = 0;
+
+		try {
+			String sql = prop.getProperty("nextBoardNo");
+
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery(); // select 수행
+
+			if (rs.next()) { // 조회 결과 1행 밖에 없음
+				boardNo = rs.getInt(1); // 첫 번째 컬럼값을 얻어와 boardNo에 저장
+			}
+
+		} finally {
+			close(rs);
+			close(pstmt);
+
+		}
+
+		return boardNo;
+	}
+
+	/**
+	 * 게시글 검색
+	 * 
+	 * @param conn
+	 * @param condition
+	 * @param query
+	 * @return boardList
+	 * @throws Exception
+	 */
+	public List<Board> searchBoard(Connection conn, int condition, String query) throws Exception {
+
+		List<Board> boardList = new ArrayList<>();
+
+		try {
+			String sql = prop.getProperty("searchBoard1") + prop.getProperty("searchBoard2_" + condition)
+					+ prop.getProperty("searchBoard3");
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, query);
+
+			// 3번 (제목+내용)은 ?가 3개 존재하기 때문에 추가 세팅 구문 작성
+			if (condition == 3) pstmt.setString(2, query);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+
+				int boardNo = rs.getInt("BOARD_NO");
+				String boardTitle = rs.getString("BOARD_TITLE");
+				String memberName = rs.getString("MEMBER_NM");
+				int readCount = rs.getInt("READ_COUNT");
+				String createDate = rs.getString("CREATE_DT");
+				int commentCount = rs.getInt("COMMENT_COUNT");
+
+				Board board = new Board();
+				board.setBoardNo(boardNo);
+				board.setBoardTitle(boardTitle);
+				board.setMemberName(memberName);
+				board.setReadCount(readCount);
+				board.setCreateDate(createDate);
+				board.setCommentCount(commentCount);
+
+				boardList.add(board);
+
+			}
+
+		} finally {
+			close(rs);
+			close(pstmt);
+		}
+
+		return boardList;
+	}
 
 }
--- member_board/src/edu/kh/jdbc/board/view/BoardView.java
+++ member_board/src/edu/kh/jdbc/board/view/BoardView.java
@@ -49,9 +49,11 @@ public class BoardView {
 					selectBoard();
 					break; // 게시글 상세 조회
 				case 3:
-					break;
+					insertBoard();
+					break; // 게시글 작성
 				case 4:
-					break;
+					searchBoard();
+					break; // 게시글 검색
 				case 0:
 					System.out.println("[로그인 메뉴로 이동합니다.]");
 					break;
@@ -186,16 +188,17 @@ public class BoardView {
 			case 0:
 				System.out.println("\n[게시판 메뉴로 돌아갑니다...]\n");
 				break;
-			case 4: case 5: // 4또는 5 입력 시
+			case 4:
+			case 5: // 4또는 5 입력 시
 
 				// 4 또는 5를 입력한 회원이 게시글 작성자인 경우
 				if (MainView.loginMember.getMemberNo() == board.getMemberNo()) {
 
-					if(input == 4) {
+					if (input == 4) {
 						// 게시글 수정 호출
 						updateBoard(board.getBoardNo());
 					}
-					if(input == 5) {
+					if (input == 5) {
 						// 게시글 삭제 호출
 						deleteBoard(board.getBoardNo());
 					}
@@ -458,6 +461,7 @@ public class BoardView {
 
 	/**
 	 * 게시글 삭제
+	 * 
 	 * @param boardNo
 	 */
 	private void deleteBoard(int boardNo) {
@@ -476,13 +480,127 @@ public class BoardView {
 					System.out.println("\n[게시글 삭제 실패...]\n");
 				}
 			} else {
-				System.out.println("\n[게시글 삭제 실]\n");
+				System.out.println("\n[게시글 삭제 실패]\n");
 			}
 
 		} catch (Exception e) {
 			System.out.println("\n<<게시글 삭제 중 예외 발생>>\n");
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * 게시글 등록(삽입)
+	 */
+	private void insertBoard() {
+
+		try {
+			System.out.println("\n[게시글 등록]\n");
+
+			System.out.print("제목 : ");
+			String boardTitle = sc.nextLine();
+
+			System.out.println("내용 : ");
+			String boardContent = inputContent();
+
+			// Board 객체에 제목, 내용, 회원 번호를 담아서 서비스에 전달
+			Board board = new Board();
+			board.setBoardTitle(boardTitle);
+			board.setBoardContent(boardContent);
+			board.setMemberNo(MainView.loginMember.getMemberNo());
+
+			int result = bService.insertBoard(board);
+			// 0 또는 생성된 게시글 번호(0 초과)
+
+			if (result > 0) {
+				System.out.println("\n[게시글이 등록되었습니다.]\n");
+
+				// 게시글 상세 조회 서비스 호출 후 결과 반환 받기
+				Board b = bService.selectBoard(result, MainView.loginMember.getMemberNo());
+				// 게시글번호, 로그인한 회원의 회원번호
+				//
+				if (b != null) {
+					System.out.println("--------------------------------------------------------");
+					System.out.printf("글번호 : %d \n제목 : %s\n", b.getBoardNo(), b.getBoardTitle());
+					System.out.printf("작성자 : %s | 작성일 : %s  \n조회수 : %d\n", b.getMemberName(), b.getCreateDate(),
+							b.getReadCount());
+					System.out.println("--------------------------------------------------------\n");
+					System.out.println(b.getBoardContent());
+					System.out.println("\n--------------------------------------------------------");
+
+					// 댓글 목록
+					if (!b.getCommentList().isEmpty()) {
+						for (Comment c : b.getCommentList()) {
+							System.out.printf("댓글번호: %d   작성자: %s  작성일: %s\n%s\n", c.getCommentNo(), c.getMemberName(),
+									c.getCreateDate(), c.getCommentContent());
+							System.out.println(" --------------------------------------------------------");
+						}
+					}
+
+					// 댓글 등록, 수정, 삭제
+					// 수정/삭제 메뉴
+					subBoardMenu(b);
+
+				} else {
+					System.out.println("[\n해당 번호의 게시글이 존재하지 않습니다.]\n");
+				}
+
+			} else {
+				System.out.println("\n[게시글 등록 실패]\n");
+			}
+
+		} catch (Exception e) {
+			System.out.println("\n<<게시글 등록 중 예외 발생>>\n");
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 게시글 검색
+	 */
+	private void searchBoard() {
+
+		try {
+			System.out.println("\n[게시글 검색]\n");
+
+			System.out.println("1) 제목");
+			System.out.println("2) 내용");
+			System.out.println("3) 제목 + 내용");
+			System.out.println("4) 작성자");
+			System.out.print("검색 조건 선택 : ");
+
+			int condition = sc.nextInt();
+			sc.nextLine();
+
+			if (condition >= 1 && condition <= 4) { // 정상 입력
+
+				System.out.print("검색어 입력 : ");
+				String query = sc.nextLine();
+
+				// 검색 서비스 호출 후 결과 반환 받기
+				List<Board> boardList = bService.searchBoard(condition, query);
+
+				if (boardList.isEmpty()) { // 조회 결과가 없을 경우
+					System.out.println("\n[검색 결과가 없습니다.]\n");
+				} else {
+					for (Board b : boardList) {
+
+						// 3 | 샘플 제목3[4] | 유저삼 |
+						System.out.printf("%d | %s[%d] | %s | %s | %d\n", b.getBoardNo(), b.getBoardTitle(),
+								b.getCommentCount(), b.getMemberName(), b.getCreateDate(), b.getReadCount());
+					}
+				}
+
+			} else { // 비정상 입력
+				System.out.println("\n[1~4번 사이의 숫자를 입력해주세요]\n");
+			}
+
+		} catch (Exception e) {
+			System.out.println("\n<<게시글 등록 중 예외 발생>>\n");
+			e.printStackTrace();
+		}
+
 	}
 
 }
